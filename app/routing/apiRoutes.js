6187f24cba499417eaec05827e486ba8cb731bcc
mcarreon <carryon2095@gmail.com>
2018-08-27 20:10:41 -0700

    changed compatFriends to take body-parser scores, cleaned up console logs for navigation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,14 +4,23 @@ var friendsArray = require("../data/friends.js");
 module.exports = function(app) {
     
     app.get("/api/friends", function(req, res) {
+
+        console.log('\n-----');
+        console.log(`Data has been served.`);
+        console.log('-----\n');
+
         res.json(friendsArray);
     });
 
     app.post("/api/friends", function(req, res) {
-        //TODO: need to fix dummyUser, so func takes req body
+        //DONE TODO: need to fix dummyUser, so func takes req body
         var specialestFriend = compatibilityFriends(req);
         friendsArray.push(req.body);
         res.json(specialestFriend);
+
+        console.log('\n-----');
+        console.log(`Posting survey data.`);
+        console.log('-----\n');
         
         //console.log(friendsArray);
     });
@@ -37,27 +46,31 @@ function compatibilityProto() {
 
 
 function compatibilityFriends(req) {
-    //TODO change dummy user to take in req.body with bodyparser
-    var dummyUser = [3, 5, 2, 4, 1, 2, 3, 4, 1, 3];
+    //DONE TODO: change dummy user to take in req.body with bodyparser
+    //var dummyUser = [3, 5, 2, 4, 1, 2, 3, 4, 1, 3];
     //var dummyUser = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];    
     //console.log(friendsArray);
 
+    var userScore = req.body.scores;
     var currentTop;
     var currentTopScore = 404;
 
     friendsArray.forEach(function (e) {
         
         var compatibilityScore = 0;
-        for (var i = 0; i < dummyUser.length; i++) {
-            compatibilityScore += Math.abs(parseInt(dummyUser[i]) - parseInt(e.scores[i]));
+        for (var i = 0; i < userScore.length; i++) {
+            compatibilityScore += Math.abs(parseInt(userScore[i]) - parseInt(e.scores[i]));
         }
         if (compatibilityScore < currentTopScore) {
             currentTopScore = compatibilityScore;
             currentTop = e;
         }
+
+        console.log('\n-----');
         console.log(`Who's the Specialest Right Now? It's ${currentTop.name}`);
         console.log(`Total Compatibility Score with ${e.name}: ${compatibilityScore}`);
         console.log(`${currentTop.name} has the highlowest score at ${currentTopScore}`);
+        console.log('-----\n');
     });
 
     return currentTop;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,10 +4,16 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/survey", function(req, res) {
+        console.log('\n-----');
+        console.log(`Do you have time to fill out this quick survey?`);
+        console.log('-----\n');
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     app.get("*", function(req, res) {
+        console.log('\n-----');
+        console.log(`Honey I'm home.`);
+        console.log('-----\n');
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
